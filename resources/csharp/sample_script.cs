e0039d9ba62172e49a28728fcbec9118dd5ff1a6
Yoni Leibowitz <yonil@microsoft.com>
2019-07-31 13:49:14 -0700

    Update sample_script.cs

--- resources/csharp/sample_script.cs
+++ resources/csharp/sample_script.cs
@@ -1,9 +1,12 @@
-result = df; 
-var n = df.Rows.Count; 
-var g = int.Parse(kargs["gain"]); 
-var f = int.Parse(kargs["cycles"]); 
-result.Columns.Add(new DataColumn("fx", typeof(double))); 
-foreach (var row in result.AsEnumerable()) 
-{ 
-    row["fx"] = g * Math.Sin((long)row["x"] / (double)n * 2 * Math.PI * f); 
+public DataTable Process(DataTable inputTable, Dictionary<string, string> args)
+{
+	DataTable output = df; 
+	var n = df.Rows.Count; 
+	var g = int.Parse(kargs["gain"]); 
+	var f = int.Parse(kargs["cycles"]); 
+	output.Columns.Add(new DataColumn("fx", typeof(double))); 
+	foreach (var row in output.AsEnumerable()) 
+	{ 
+		row["fx"] = g * Math.Sin((long)row["x"] / (double)n * 2 * Math.PI * f); 
+	}
 }
