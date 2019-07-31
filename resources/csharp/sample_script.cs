4f83733d96242b798c3b8259f0812acaaf8972ed
Yoni Leibowitz <yonil@microsoft.com>
2019-07-31 14:07:28 -0700

    Update sample_script.cs

--- resources/csharp/sample_script.cs
+++ resources/csharp/sample_script.cs
@@ -2,8 +2,8 @@ public DataTable Process(DataTable inputTable, Dictionary<string, string> args)
 {
 	DataTable output = df; 
 	var n = df.Rows.Count; 
-	var g = int.Parse(kargs["gain"]); 
-	var f = int.Parse(kargs["cycles"]); 
+	var g = int.Parse(args["gain"]); 
+	var f = int.Parse(args["cycles"]); 
 	output.Columns.Add(new DataColumn("fx", typeof(double))); 
 	foreach (var row in output.AsEnumerable()) 
 	{ 
